23461878d7d51664411bbb6d113a5a1413b6581c
Fred Block <info@fredblock.com>
2015-08-04 14:59:17 -0600

    Update 'index.js' with dataLoop. WIP...
    modified:   dataMock.js
    modified:   index.html
    modified:   index.js
    new file:   junk.js
    new file:   notes.txt

--- dataMock.js
+++ dataMock.js
@@ -1,21 +1,48 @@
-var io   = require('socket.io')();
-var socket = io();
+var app  = require('express')();
+var http = require('http').Server(app);
+var io   = require('socket.io')(http);
+// var socket = io();
 
-var i = 1;
-
-function dataLoop() {
-  setTimeout(function () {
-    console.log('Temperature: 77');
-    socket.emit('temperature-logged', '77 Fahrenheit');
-    i++;
-    if (i < 10) {
-      dataLoop();
-    }
-  }, 1000)
-}
-
-dataLoop();
+function DataMock() {};
 
 
+io.on('connection', function(socket) {
 
-// module.exports = dataMock;
+  DataMock.prototype.dataLoop = function () {
+    setTimeout(function () {
+      var i = 1;
+      console.log('temperature-logged ' + '77°F');
+      socket.emit('temperature-logged', '77°F');
+      i++;
+      if (i < 10) {
+        this.dataLoop();
+      }
+    }, 1000)
+  };
+
+  socket.on('temperature-logged', function (temp) {
+    console.log('Temperature: ' + temp);
+    io.emit('temperature-logged', temp);
+  });
+
+});
+
+
+// var i = 1;
+// var temp = 75;
+// function dataLoop() {
+//   setTimeout(function () {
+//     console.log('temperature-logged: ' + temp)
+//     socket.emit('temperature-logged', temp + '°F');
+//     i++;
+//     temp++;
+//     if (i < 10) {
+//       dataLoop();
+//     }
+//   }, 1000)
+// };
+// dataLoop();
+
+
+
+module.exports = DataMock;
--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 var app  = require('express')();
 var http = require('http').Server(app);
 var io   = require('socket.io')(http);
+// var DataMock = require('./dataMock');
+// var data = new DataMock();
+
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+
 io.on('connection', function(socket) {
 
-  // PUT TEMP LOOPER HERE???
   var i = 1;
-
+  var temp = 75;
   function dataLoop() {
     setTimeout(function () {
-      console.log('temperature-logged ' + '77°F');
-      socket.emit('temperature-logged', '77°F');
+      console.log('temperature-logged: ' + temp)
+      socket.emit('temperature-logged', temp + '°F');
       i++;
+      temp++;
       if (i < 10) {
         dataLoop();
       }
     }, 1000)
-  }
-
+  };
   dataLoop();
 
 
@@ -34,6 +37,7 @@ io.on('connection', function(socket) {
     console.log('user disconnected');
   });
 });
+
 
 http.listen(3000, function() {
   console.log('listening on *:3000');
