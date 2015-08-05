c3bcae50a57fc7fad891ef0fbe0ddcc2d2693db5
Fred Block <info@fredblock.com>
2015-08-05 05:25:56 -0600

    Push logic down the stack. Remove dataLoop and socket events and put in 'dataMock.js'.
    modified:   dataMock.js
    modified:   index.html
    modified:   index.js

--- dataMock.js
+++ dataMock.js
@@ -1,48 +1,40 @@
-var app  = require('express')();
-var http = require('http').Server(app);
-var io   = require('socket.io')(http);
-// var socket = io();
+module.exports = function (io) {
 
-function DataMock() {};
+  io.on('connection', function(socket) {
+
+    var i = 1;
+    var temp = 75;
+    function dataLoop() {
+      setTimeout(function () {
+        console.log('temperature-logged: ' + temp)
+        socket.emit('temperature-logged', temp + '°F');
+        i++;
+        temp++;
+        if (i < 10) {
+          dataLoop();
+        };
+        if (temp > 80) {
+          console.log('temperature-too-high');
+          socket.emit('temperature-too-high');
+        };
+      }, 1000)
+    };
+    dataLoop();
 
 
-io.on('connection', function(socket) {
 
-  DataMock.prototype.dataLoop = function () {
-    setTimeout(function () {
-      var i = 1;
-      console.log('temperature-logged ' + '77°F');
-      socket.emit('temperature-logged', '77°F');
-      i++;
-      if (i < 10) {
-        this.dataLoop();
-      }
-    }, 1000)
-  };
+    socket.on('temperature-logged', function (temp) {
+      console.log('Temperature: ' + temp);
+      io.emit('temperature-logged', temp);
+    });
 
-  socket.on('temperature-logged', function (temp) {
-    console.log('Temperature: ' + temp);
-    io.emit('temperature-logged', temp);
+    socket.on('temperature-too-high', function () {
+      io.emit('temperature-too-high');
+    });
+
+    // socket.on('disconnect', function () {
+    //   console.log('user disconnected');
+    // });
   });
 
-});
-
-
-// var i = 1;
-// var temp = 75;
-// function dataLoop() {
-//   setTimeout(function () {
-//     console.log('temperature-logged: ' + temp)
-//     socket.emit('temperature-logged', temp + '°F');
-//     i++;
-//     temp++;
-//     if (i < 10) {
-//       dataLoop();
-//     }
-//   }, 1000)
-// };
-// dataLoop();
-
-
-
-module.exports = DataMock;
+};
--- index.js
+++ index.js
@@ -1,44 +1,59 @@
-var app  = require('express')();
-var http = require('http').Server(app);
-var io   = require('socket.io')(http);
-// var DataMock = require('./dataMock');
-// var data = new DataMock();
+const app  = require('express')();
+const http = require('http').Server(app);
+const io   = require('socket.io')(http);
+
+const dataMock = require('./dataMock')(io);
 
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});
+
 
 io.on('connection', function(socket) {
 
-  var i = 1;
-  var temp = 75;
-  function dataLoop() {
-    setTimeout(function () {
-      console.log('temperature-logged: ' + temp)
-      socket.emit('temperature-logged', temp + '°F');
-      i++;
-      temp++;
-      if (i < 10) {
-        dataLoop();
-      }
-    }, 1000)
-  };
-  dataLoop();
+  // var i = 1;
+  // var temp = 75;
+  // function dataLoop() {
+  //   setTimeout(function () {
+  //     console.log('temperature-logged: ' + temp)
+  //     socket.emit('temperature-logged', temp + '°F');
+  //     i++;
+  //     temp++;
+  //     if (i < 10) {
+  //       dataLoop();
+  //     };
+  //     if (temp > 80) {
+  //       console.log('temperature-too-high');
+  //       socket.emit('temperature-too-high');
+  //     };
+  //   }, 1000)
+  // };
+  // dataLoop();
 
 
-  socket.on('temperature-logged', function (temp) {
-    console.log('Temperature: ' + temp);
-    io.emit('temperature-logged', temp);
-  });
+  // function tempTooHigh() {
+  //   if (temp > 80) {
+  //     console.log('temperature-too-high');
+  //     socket.emit('temperature-too-high');
+  //   };
+  // };
+
+
+  // socket.on('temperature-logged', function (temp) {
+  //   console.log('Temperature: ' + temp);
+  //   io.emit('temperature-logged', temp);
+  // });
+  //
+  // socket.on('temperature-too-high', function () {
+  //   io.emit('temperature-too-high');
+  // })
 
   socket.on('disconnect', function () {
     console.log('user disconnected');
   });
-});
-
-
-http.listen(3000, function() {
-  console.log('listening on *:3000');
 });
