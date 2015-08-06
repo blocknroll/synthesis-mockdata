da394a808a7ab88e9e561c5bf53a0c345a07ffff
Fred Block <info@fredblock.com>
2015-08-06 07:29:34 -0600

    Update 'dataMock.js'. Add 'temperature-too-low' events. Update temperature display on 'index.html'
    modified:   client/client.js
    new file:   public/css/styles.scss
    modified:   public/index.html
    modified:   public/main.bundle.js
    modified:   scratch/junk.js
    modified:   server/dataMock.js

--- scratch/junk.js
+++ scratch/junk.js
@@ -1,3 +1,141 @@
+
+var temperature = [
+  { fahrenheit: 75 },
+  { fahrenheit: 76 },
+  { fahrenheit: 77 },
+]
+
+// want:
+// temperature[0][fahrenheit]
+
+// works
+console.log(temperature[0].fahrenheit);
+console.log(temperature[1].fahrenheit);
+console.log(temperature[2].fahrenheit);
+
+// works
+for (var i = 0; i < 1000; i++) {
+  console.log(temperature[i].fahrenheit);
+};
+
+
+// best so far(renheit)
+for (var i = 0, l = temperature.length; i < l; i++) {
+  var temp = temperature[i].fahrenheit;
+  console.log(temp);
+}
+
+
+// more explicit
+var i = 0;
+
+function temperatureLoop() {
+    console.log(temperature[i].fahrenheit);
+    i++;
+    if (i < temperature.length) {
+      temperatureLoop();
+    }
+}
+temperatureLoop();
+
+
+
+
+
+
+// TIMEOUTS /////////////////////////////////////////////////////////////
+
+
+// dont make functions within a loop
+for (var i = 0, l = temperature.length; i < l; i++) {
+  var temp = temperature[i].fahrenheit;
+  setTimeout(function () {
+    console.log(temp);
+  }, 3000);
+}
+
+
+// timeout not working right
+function temperatureLoop() {
+  setTimeout(function () {
+    for (var i = 0, l = temperature.length; i < l; i++) {
+      var temp = temperature[i].fahrenheit;
+      console.log(temp);
+    }
+  }, 3000);
+}
+temperatureLoop();
+
+
+
+
+
+// works! //////////////////
+var i = 0;
+
+function temperatureLoop() {
+  setTimeout(function () {
+    console.log(temperature[i].fahrenheit);
+    i++;
+    if (i < temperature.length) {
+      temperatureLoop();
+    }
+  }, 3000);
+}
+temperatureLoop();
+
+
+
+
+
+
+var temperature = [
+  { fahrenheit: 75 },
+  { fahrenheit: 76 },
+  { fahrenheit: 77 },
+  { fahrenheit: 78 },
+  { fahrenheit: 79 },
+  { fahrenheit: 80 },
+  { fahrenheit: 81 },
+  { fahrenheit: 82 },
+  { fahrenheit: 83 },
+  { fahrenheit: 84 },
+  { fahrenheit: 84 },
+  { fahrenheit: 83 },
+  { fahrenheit: 82 },
+  { fahrenheit: 81 },
+  { fahrenheit: 80 },
+  { fahrenheit: 79 },
+  { fahrenheit: 78 },
+  { fahrenheit: 77 },
+  { fahrenheit: 76 },
+  { fahrenheit: 75 },
+  { fahrenheit: 75 },
+  { fahrenheit: 75 },
+  { fahrenheit: 74 },
+  { fahrenheit: 73 },
+  { fahrenheit: 72 },
+  { fahrenheit: 71 },
+  { fahrenheit: 70 },
+  { fahrenheit: 69 },
+  { fahrenheit: 68 },
+  { fahrenheit: 67 },
+  { fahrenheit: 67 },
+  { fahrenheit: 68 },
+  { fahrenheit: 69 },
+  { fahrenheit: 70 },
+  { fahrenheit: 71 },
+  { fahrenheit: 72 },
+  { fahrenheit: 73 },
+  { fahrenheit: 74 },
+];
+
+
+
+
+
+
+
 // var temperature = [
 //   fahrenheit: 75,
 //   fahrenheit: 76,
@@ -22,44 +160,32 @@
 // ]
 
 
-var temperature = [
-  { fahrenheit: 75 },
-  { fahrenheit: 76 },
-  { fahrenheit: 77 },
-  { fahrenheit: 78 },
-  { fahrenheit: 79 },
-  { fahrenheit: 80 },
-  { fahrenheit: 81 },
-  { fahrenheit: 82 },
-  { fahrenheit: 83 },
-  { fahrenheit: 84 },
-  { fahrenheit: 84 },
-  { fahrenheit: 83 },
-  { fahrenheit: 82 },
-  { fahrenheit: 81 },
-  { fahrenheit: 80 },
-  { fahrenheit: 79 },
-  { fahrenheit: 78 },
-  { fahrenheit: 77 },
-  { fahrenheit: 76 },
-  { fahrenheit: 75 },
-];
 
-function logArrayElements(object) {
-  console.log(object[value]);
-}
 
-temperature.forEach(logArrayElements);
 
-// temperature.forEach( function (fahrenheit) {
-//   console.log(temperature[fahrenheit]);
-// })
 
-// for (var i = 0, l = data.temperature.length; i < l; i++) {
-//   var obj = data.temperature[i];
-//   console.log(temperature[fahrenheit]);
-// }
 
-// for (var prop in temperature) {
-//   console.log(temperature[fahrenheit]);
-// }
+
+/////////////////////////////////////////////////////////////////
+
+// ORIGINAL TEST LOOP
+// var i = 1;
+// var temp = 75;
+//
+// function dataLoopUp() {
+//   setTimeout(function () {
+//     console.log('temperature-logged: ' + temp)
+//     socket.emit('temperature-logged', temp + '°F');
+//     i++;
+//     temp++;
+//     if (i < 10) {
+//       dataLoopUp();
+//     };
+//     if (temp > 80) {
+//       console.log('temperature-too-high');
+//       socket.emit('temperature-too-high');
+//     };
+//   }, 1000)
+// };
+//
+// dataLoopUp();
