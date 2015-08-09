d3a48899ff30c8dcd9cf965ad0b15e29c2815652
Fred Block <info@fredblock.com>
2015-08-09 15:41:21 -0600

    Add mocha and initial setup tests.

---
+++ test/index-test.js
@@ -0,0 +1,12 @@
+const assert = require('chai').assert;
+
+require('./dataMock-test.js');
+
+
+describe('the test bundle', function () {
+
+  it('is wired up', function () {
+    assert(true);
+  });
+
+});
--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,26 @@ const path = require('path');
 module.exports = {
   entry: {
     main: "./client/client.js",
-    // test: "mocha!./test/index.js"
+    test: "mocha!./test/index-test.js"
   },
   output: {
     path: path.join(__dirname, '/public'),
     filename: "[name].bundle.js"
   },
+  // output: {
+  //   main: {
+  //     path: path.join(__dirname, '/public'),
+  //     filename: "[name].bundle.js"
+  //   },
+  //   test: {
+  //     path: path.join(__dirname, '/test'),
+  //     filename: "[name].bundle.js"
+  //   },
+  // },
+  // output: {
+  //   path: __dirname,
+  //   filename: "[name].bundle.js"
+  // },
   module: {
     loaders: [
       { test: /\.css$/, loader: "style!css" },
